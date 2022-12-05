8caa5a60b4d9d4053a241fde11c8d438e1a38353
Hedquist <vilhelm.hedquist@hotmail.com>
2022-12-05 01:47:27 +0100

    Day 2 done

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -6,13 +6,89 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// A X - Rock     = 1
+// B Y - Paper    = 2
+// C Z - Scissors = 3
+
+//       Loss     = 0
+//       Draw     = 3
+//       Win      = 6
+
 public class Day2 {
     public static void main(String[] args) {
-        String[] input = readInput("Day2/input.txt");
-        for (int i = 0; i < input.length; i++) {
-            System.out.println(input[i]);
-        }
+        part2();
+    }
 
+    private static void part1 () {
+        String[] input = readInput("Day2/input.txt");
+
+        int totalScore = 0;
+        char rawOpponent;
+        char rawMe;
+        int opponent;
+        int me;
+
+        for (String s : input) {
+            rawOpponent = s.charAt(0);
+            rawMe = s.charAt(2);
+
+            if (rawOpponent == 'A') {
+                opponent = 1;
+            } else if (rawOpponent == 'B') {
+                opponent = 2;
+            } else {
+                opponent = 3;
+            }
+
+            if (rawMe == 'X') {
+                me = 1;
+            } else if (rawMe == 'Y') {
+                me = 2;
+            } else {
+                me = 3;
+            }
+
+            if (opponent - me == 0) {
+                totalScore += 3 + me;
+            } else if (opponent - me == -1 || opponent - me == 2) {
+                totalScore += 6 + me;
+            } else {
+                totalScore += me;
+            }
+        }
+        System.out.println(totalScore);
+    }
+
+    private static void part2 () {
+        String[] input = readInput("Day2/input.txt");
+
+        int totalScore = 0;
+        char rawOpponent;
+        char rawResult;
+        int opponent;
+        int result;
+
+        for (String s : input) {
+            rawOpponent = s.charAt(0);
+            rawResult = s.charAt(2);
+
+            if (rawOpponent == 'A') {
+                opponent = 1;
+            } else if (rawOpponent == 'B') {
+                opponent = 2;
+            } else {
+                opponent = 3;
+            }
+
+            if (rawResult == 'X') {
+                totalScore += (opponent + 1) % 3 + 1;
+            } else if (rawResult == 'Y') {
+                totalScore += opponent + 3;
+            } else {
+                totalScore += opponent % 3 + 1 + 6;
+            }
+        }
+        System.out.println(totalScore);
     }
 
     private static String[] readInput (String filePath) {
@@ -20,7 +96,7 @@ public class Day2 {
             File inputFile = new File(filePath);
             Scanner s = new Scanner(inputFile);
 
-            List<String> outputList = new ArrayList<String>();
+            List<String> outputList = new ArrayList<>();
             while (s.hasNextLine()) {
                 outputList.add(s.nextLine());
             }
@@ -28,6 +104,7 @@ public class Day2 {
 
             s.close();
             return output;
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
